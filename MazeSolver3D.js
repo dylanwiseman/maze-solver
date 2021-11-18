df2c06243e6763983b8681220c0e741196e5ddd4
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-11-17 23:09:40 -0700

    3d maze fix

--- MazeSolver3D.js
+++ MazeSolver3D.js
@@ -29,7 +29,7 @@ let myMaze = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1, 0, 0, 1, 1],
     [0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
     [0, 1, 1, 1, 1, 0, 1, 1, 1, 0],
     [0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
@@ -102,9 +102,9 @@ let myMaze = [
     [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
     [1, 1, 1, 0, 1, 0, 0, 1, 1, 1],
     [0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 1, 1, 0, 1, 0, 1, 1, 1, 0],
-    [0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 1, 0, 1, 0, 0, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ],
@@ -153,26 +153,32 @@ function MazeSolver(maze) {
       );
       this.path.push([depth, column, row]);
       this.maze[depth][column][row] = 9;
-      if (depth < this.maze.length - 1) {
+      if (depth < this.maze.length - 1 && !this.solved) {
+        console.log("trying deeper");
         this.traverse(depth + 1, column, row);
       }
-      if (column < this.maze[depth].length - 1) {
+      if (column < this.maze[depth].length - 1 && !this.solved) {
+        console.log("trying down");
         this.traverse(depth, column + 1, row);
       }
-      if (row < this.maze[depth][column].length - 1) {
+      if (row < this.maze[depth][column].length - 1 && !this.solved) {
+        console.log("trying right");
         this.traverse(depth, column, row + 1);
       }
-      if (column > 0) {
-        this.traverse(column - 1, row);
+      if (column > 0 && !this.solved) {
+        console.log("trying left");
+        this.traverse(depth, column - 1, row);
       }
-      if (row > 0) {
-        this.traverse(column, row - 1);
+      if (row > 0 && !this.solved) {
+        console.log("trying up");
+        this.traverse(depth, column, row - 1);
       }
-      if (depth > 0) {
+      if (depth > 0 && !this.solved) {
+        console.log("trying shallower");
         this.traverse(depth - 1, column, row);
       }
       if (!this.solved) {
-        console.log("backtracking from ", depth, column, row);
+        console.log("no options, backtracking from ", depth, column, row);
         this.path.pop();
       }
     }

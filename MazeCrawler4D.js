912fc721f7acffcc633c49730d7bbad06a9d4cf2
Dylan Wiseman <85514881+dylanwiseman@users.noreply.github.com>
2022-04-26 19:28:33 -0600

    Update MazeCrawler4D.js
    function instead of class

--- MazeCrawler4D.js
+++ MazeCrawler4D.js
@@ -1,10 +1,10 @@
 //Copy everything and run in Replit to see the MazeCrawler in motion:
 
-//Interdimensional MazeCrawler Class:
-function MazeCrawler(maze) {
-  this.maze = maze;
-  this.solved = false;
-  this.path = [];
+//Interdimensional MazeCrawler Function:
+function mazeCrawler(maze, start) {
+
+  let solved = false;
+  const path = [];
 
   //Our MazeCrawler uses a Depth-First approach to solving the maze,
   //following one path at a time until it finds success or a dead end.
@@ -14,71 +14,71 @@ function MazeCrawler(maze) {
   // (3) - custom action, in this case, a portal to another maze
   // (1) - a valid spot, then traverse calls itself (the same traverse function), passing in coordinates of the surrounding spots
 
-  this.traverse = function (universe, depth, column, row) {
+  const traverse = (universe, depth, column, row) => {
     // If MazeCrawler finds a 2, we're done!
-    if (this.maze[universe][depth][column][row] === 2) {
+    if (maze[universe][depth][column][row] === 2) {
       //adding coordinates to the path array:
-      this.path.push([universe, depth, column, row]);
+      path.push([universe, depth, column, row]);
       console.log(
         `MazeCrawler finished the maze at Universe: ${universe}, Depth: ${depth}, Col: ${column}, Row: ${row}`
       );
-      this.solved = true;
+      solved = true;
       console.log(
         "Here's the path it took (Universe, Depth, Col, Row): ",
-        this.path
+        path
       );
-    } else if (this.maze[universe][depth][column][row] === 3 && !this.solved) {
+    } else if (maze[universe][depth][column][row] === 3 && !solved) {
       // If MazeCrawler finds a 3, we're going to travel to another Maze in the Maze-verse by passing in special coordinates to traverse:
       console.log(
         `Found a mysterious portal at Universe: ${universe}, Depth: ${depth}, Col: ${column}, Row: ${row}... MazeCrawler's going through it!`
       );
-      this.path.push([universe, depth, column, row]);
+      path.push([universe, depth, column, row]);
       // Sending MazeCrawler to the alternate universe, or back to the original:
-      this.maze[universe][depth][column][row] = 9;
+      maze[universe][depth][column][row] = 9;
       universe === 0 ? (universe = 1) : (universe = 0);
       depth = 0;
-      this.traverse(universe, depth, column, row);
-    } else if (this.maze[universe][depth][column][row] === 1 && !this.solved) {
+      traverse(universe, depth, column, row);
+    } else if (maze[universe][depth][column][row] === 1 && !solved) {
       // If MazeCrawler finds a one, we're going to try traversing from there:
       console.log(
         `Traversed to: (Universe: ${universe}, Depth: ${depth}, Col: ${column}, Row: ${row})`
       );
-      this.path.push([universe, depth, column, row]);
+      path.push([universe, depth, column, row]);
       // First we set the 1 at our current location to 9, so we can't traverse it again:
-      this.maze[universe][depth][column][row] = 9;
+      maze[universe][depth][column][row] = 9;
       // We'll try going deeper first by calling the traverse function with our coordinates adjusted by depth + 1:
-      if (depth < this.maze[universe].length - 1 && !this.solved) {
+      if (depth < maze[universe].length - 1 && !solved) {
         console.log("trying deeper");
-        this.traverse(universe, depth + 1, column, row);
+        traverse(universe, depth + 1, column, row);
       }
       // If we can't go deeper, we'll try moving down one, again calling traverse with column + 1:
-      if (column < this.maze[universe][depth].length - 1 && !this.solved) {
+      if (column < maze[universe][depth].length - 1 && !solved) {
         console.log("trying down");
-        this.traverse(universe, depth, column + 1, row);
+        traverse(universe, depth, column + 1, row);
       }
       // Then to the right, traverse with row + 1:
-      if (row < this.maze[universe][depth][column].length - 1 && !this.solved) {
+      if (row < maze[universe][depth][column].length - 1 && !solved) {
         console.log("trying right");
-        this.traverse(universe, depth, column, row + 1);
+        traverse(universe, depth, column, row + 1);
       }
       // To the left:
-      if (column > 0 && !this.solved) {
+      if (column > 0 && !solved) {
         console.log("trying left");
-        this.traverse(universe, depth, column - 1, row);
+        traverse(universe, depth, column - 1, row);
       }
       // Maybe we can go up?
-      if (row > 0 && !this.solved) {
+      if (row > 0 && !solved) {
         console.log("trying up");
-        this.traverse(universe, depth, column, row - 1);
+        traverse(universe, depth, column, row - 1);
       }
       // Our last attempt to traverse, we try moving shallower:
-      if (depth > 0 && !this.solved) {
+      if (depth > 0 && !solved) {
         console.log("trying shallower");
-        this.traverse(universe, depth - 1, column, row);
+        traverse(universe, depth - 1, column, row);
       }
       // If none of those are available, the traverse function that called us is done,
       // and the traverse function that called THAT traverse function will then try traversing in the next direction.
-      if (!this.solved) {
+      if (!solved) {
         console.log(
           "no options, backtracking from ",
           universe,
@@ -87,10 +87,11 @@ function MazeCrawler(maze) {
           row
         );
         // Lastly, we remove the location we just tried from our path array:
-        this.path.pop();
+        path.pop();
       }
     }
   };
+  traverse(...start);
 }
 
 //Our Multiversal Maze:
@@ -247,6 +248,7 @@ let theMaze = [
 ];
 
 //Starting coordinates tell the crawler where to start:
-let startingCoordinates = [0, 0, 3, 0];
-let crawler = new MazeCrawler(theMaze);
-crawler.traverse(...startingCoordinates);
+let start = [0, 0, 3, 0];
+
+
+mazeCrawler(theMaze, start);
